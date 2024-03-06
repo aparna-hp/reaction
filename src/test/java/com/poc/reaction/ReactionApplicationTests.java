82d279019f17ee24035de243502b2dce991728a0
aparp <aparp@cisco.com>
2024-03-06 20:55:13 +0530

    Base folders for Reaction microservice

---
+++ src/test/java/com/poc/reaction/ReactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.reaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
